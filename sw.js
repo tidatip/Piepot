4bb67f6185c9f825ff2e5d037094fbcc0201fcf5
tidatip <tidatip.lee@gmail.com>
2026-08-27 18:08:37 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'piepot-v1';
+const CACHE = 'piepot-v2';
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(['./', './index.html', './manifest.webmanifest'])).then(() => self.skipWaiting()));
 });
